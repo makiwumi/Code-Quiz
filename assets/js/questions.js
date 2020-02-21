6fa73e67aa1bc900b23755ad4aec6d94c3b81654
Marian Akiwumi <mfakiwumi1992@yahoo.com>
2020-02-21 12:21:17 -0500

    view questions functions

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,42 +1,49 @@
 // set variables/array (set questions) - loop thru questions and right answer (jsun)
 
 let questions = [{
-	"question":"this is a question 1",
-	"answers":"['a', 'b']",
-	"correctAnswer":"b"},
+	question:"this is a question 1",
+	options:"['a', 'b']",
+	correctAnswer:"b"},
 	{
-	"question":"this is a question 2",
-	"answers":"['c', 'd']",
-	"correctAnswer":"c"},
+    question:"this is a question 1",
+    options:"['a', 'b']",
+    correctAnswer:"b"},
 	{
-	"question":"this is a question 3",
-	"answers":"['answer1', 'answer2']",
-	"correctAnswer":"answer2"},
+    question:"this is a question 1",
+    options:"['a', 'b']",
+    correctAnswer:"b"},
 	{
-	"question":"this is a question 4",
-	"answers":"['answer1', 'answer2']",
-	"correctAnswer":"answer2"},
+    question:"this is a question 1",
+    options:"['a', 'b']",
+    correctAnswer:"b"},
 	{
-	"question":"this is a question 5",
-	"answers":"['answer1', 'answer2']",
-	"correctAnswer":"answer2"}
+    question:"this is a question 1",
+    options:"['a', 'b']",
+    correctAnswer:"b"}
 ];
 console.log(questions);
 // on click function using start button
-var secondsLeft = 60;
+let secondsLeft = 60;
+
 start.onclick = function(){
     //start time, show first question, list of ans.
-    var mainEl = document.getElementById("time");
-    var timerInterval = setInterval(function() {
+    let mainEl = document.getElementById("time");
+    let timerInterval = setInterval(function() {
         secondsLeft--;
         mainEl.textContent = secondsLeft;
     if(secondsLeft === 0) {
       clearInterval(timerInterval);
     }
-    }, 750);
+    }, 1000);
     console.log("start");
+
+    // Call question function 
+    showQuestion();
 }
 //function that loops thru ques array 1 at a time
+function showQuestion(){
+    let currentQuestion = questions
+}
 //function to compare ans (right or wrong)
 //function end screen
     //user chooses ans. 
